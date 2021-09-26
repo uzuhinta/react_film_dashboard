8f1b27ec88ce388bcae981d0e52e0031586658a1
Ba Quan Win10 <quannar178@gmail.com>
2021-09-27 01:28:28 +0700

    Remove redundance element in App

--- src/App.js
+++ src/App.js
@@ -1,69 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import LangSwitcher from 'features/i18n/components/LangSwitcher';
-import { ToggleTheme } from 'features/theme/components/ToggleTheme';
+import { selectAuth } from 'features/auth/authSlice';
 import LoginWithGoogle from 'features/auth/components/LoginWithGoogle';
 import SignOutWithGoogle from 'features/auth/components/SignOutWithGoogle';
+import LangSwitcher from 'features/i18n/components/LangSwitcher';
+import { ToggleTheme } from 'features/theme/components/ToggleTheme';
+import React from 'react';
 import { useSelector } from 'react-redux';
-import { selectAuth } from 'features/auth/authSlice';
-import routes from 'routes/routes';
-import { useRoutes } from 'react-router-dom';
 import ThemeRoutes from 'routes/routes';
+import './App.css';
+import logo from './logo.svg';
 
 function App() {
     const auth = useSelector(selectAuth);
     console.log('In start app: ');
     return (
         <div className="App bg-primary transition-all duration-300">
-            <header className="App-header">
-                <img src={logo} className="App-logo " alt="logo" />
-                <LangSwitcher />
-                <ToggleTheme />
-                {auth.user ? <SignOutWithGoogle /> : <LoginWithGoogle />}
-                <ThemeRoutes />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <span>
-                    <span>Learn </span>
-                    <a
-                        className="App-link "
-                        href="https://reactjs.org/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        React
-                    </a>
-                    <span>, </span>
-                    <a
-                        className="App-link"
-                        href="https://redux.js.org/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Redux
-                    </a>
-                    <span>, </span>
-                    <a
-                        className="App-link"
-                        href="https://redux-toolkit.js.org/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Redux Toolkit
-                    </a>
-                    ,<span> and </span>
-                    <a
-                        className="App-link"
-                        href="https://react-redux.js.org/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        React Redux
-                    </a>
-                </span>
-            </header>
+            <LangSwitcher />
+            <ToggleTheme />
+            {auth.user ? <SignOutWithGoogle /> : <LoginWithGoogle />}
+            <ThemeRoutes />
         </div>
     );
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,8 @@
 import { selectAuth } from 'features/auth/authSlice';
-import Login from 'features/auth/pages/Login';
-import { Counter } from 'features/counter/Counter';
 import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 import { AdminRoutes } from './AdminRoutes';
 import { PublicRoutes } from './PublicRoutes';
-import { useRoutes } from 'react-router-dom';
 
 function ThemeRoutes() {
     const auth = useSelector(selectAuth);
