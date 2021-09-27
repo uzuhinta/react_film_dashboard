97282b41cbf67ec7b277ff926d8d1e76a059da3e
Ba Quan Win10 <quannar178@gmail.com>
2021-09-27 22:37:43 +0700

    Add translate for auth

--- src/features/auth/components/LoginWithGoogle.jsx
+++ src/features/auth/components/LoginWithGoogle.jsx
@@ -1,4 +1,5 @@
 import { Button } from '@mui/material';
+import { selectTranslations } from 'features/i18n/i18nSlice';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { googleSignInStart } from '../authSlice';
@@ -6,20 +7,21 @@ import { googleSignInStart } from '../authSlice';
 function LoginWithGoogle() {
     const dispatch = useDispatch();
     const authState = useSelector((state) => state.auth);
+    const t = useSelector(selectTranslations);
     const handleClick = () => {
         console.log('Handle signin click');
         dispatch(googleSignInStart());
     };
     return (
-            <Button onClick={handleClick}>
-                {authState.status === 'loading' ? (
-                    <div className="animate-spin rounded-full h-6 w-6 border-t-2 border-b-2 border-purple-500 inline-block"></div>
-                ) : null}
-                Login with Google
-                {/* {authState.status === 'error'
+        <Button onClick={handleClick}>
+            {authState.status === 'loading' ? (
+                <div className="animate-spin rounded-full h-6 w-6 border-t-2 border-b-2 border-gray-600 inline-block"></div>
+            ) : null}
+            {t.auth.signIn}
+            {/* {authState.status === 'error'
                     ? 'Try login with google again'
                     : ''} */}
-            </Button>
+        </Button>
     );
 }
 
--- src/features/auth/components/SignOutWithGoogle.jsx
+++ src/features/auth/components/SignOutWithGoogle.jsx
@@ -1,16 +1,19 @@
 import { Button } from '@mui/material';
+import { selectTranslations } from 'features/i18n/i18nSlice';
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { googleSignOut } from '../authSlice';
 
 function SignOutWithGoogle() {
     const dispatch = useDispatch();
+    const t = useSelector(selectTranslations);
+    console.log({ t });
     const handleSignOut = () => {
         dispatch(googleSignOut());
     };
     return (
         <Button variant="outlined" color="warning" onClick={handleSignOut}>
-            Sign out
+            {t.auth.signOut}
         </Button>
     );
 }
